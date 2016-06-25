a0d568ae66377e2bc8431eb3a10347546247dc50
Jon Sharratt <jon@craftship.io>
2016-06-25 16:31:09 +0100

    Add sinon and sinon chai setup
    Required when we test subscribers and listeners.

---
+++ flow/sinon.js
@@ -0,0 +1,1 @@
+declare var sinon: Function;
--- test.setup.js
+++ test.setup.js
@@ -1,4 +1,9 @@
 import 'babel-polyfill';
 import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
 
 global.expect = chai.expect;
+global.sinon = sinon;
